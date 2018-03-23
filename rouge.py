4ffcb8bf9243527efcd804b785cb4f075e925f3d
mattheuslee <mattheus.lee@gmail.com>
2018-03-23 15:57:09 +0800

    Add conditional to avoid division by 0

--- rouge.py
+++ rouge.py
@@ -22,14 +22,18 @@ class ROUGE:
         for key in reference_n_grams.keys():
             if key in generated_n_grams:
                 recall_hit += reference_n_grams[key]
-        recall_score = recall_hit / (len(reference) + 1e-2)
+        recall_score = 0.0
+        if len(reference) != 0:
+            recall_score = recall_hit / len(reference)
 
         # Calculate precision
         precision_hit = 0.0
         for key in generated_n_grams.keys():
             if key in reference_n_grams:
                 precision_hit += generated_n_grams[key]
-        precision_score = precision_hit / (len(generated) + 1e-2)
+        precision_score = 0.0
+        if len(generated) != 0:
+            precision_score = precision_hit / len(generated)
         
         return {"recall"   : recall_score,
                 "precision": precision_score}
