4990e8bd45f68c2f42d772ff92bf6506f7cf8ee2
mattheuslee <mattheus.lee@gmail.com>
2018-03-25 22:08:08 +0800

    Remove echo

--- batch_1000/download.sh
+++ batch_1000/download.sh
@@ -2,6 +2,5 @@
 mkdir batch_1000
 for i in {0..79}
 do
-    echo $i/79
     wget -O batch_1000/cnn_stories_80000_$i.txt https://raw.githubusercontent.com/mattheuslee/Deep-Reinforced-Abstractive-Summarization/master/batch_1000/cnn_stories_80000_$i.txt -q
 done
